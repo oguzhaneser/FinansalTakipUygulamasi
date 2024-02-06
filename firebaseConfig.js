9eeed6fc28725474547858a1073519357d5c85fd
Oğuzhan Eşer <oguzhan.eser@hotmail.com>
2024-02-06 18:11:06 +0300

    firebase sdk & firebaseConfig.js eklendi.

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0xGwDTzKkxIAvcJxLgqtMqNrKiG8a4nY",
+    authDomain: "finansaltakipuygulamasi.firebaseapp.com",
+    projectId: "finansaltakipuygulamasi",
+    storageBucket: "finansaltakipuygulamasi.appspot.com",
+    messagingSenderId: "408081340187",
+    appId: "1:408081340187:web:e3c545c769762b439565be"
+  };
